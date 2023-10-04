edc43d3890134ac00297d80d0302359a9c81e013
Stepan Krasicov <kripdok@gmail.com>
2023-10-04 09:55:03 +0700

    Created a class responsible for vision

---
+++ Assets/Scripts/Character/FieldOfView.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [Range(0, 360)]
+    [SerializeField] private int _angleOfView;
+    [SerializeField] private LayerMask _targetMask;
+    [SerializeField] private LayerMask _obstructionMask;
+
+    private Transform _target;
+
+    public event UnityAction<Transform> TargetHasBeenSighted;
+
+    private void Update()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, _radius, _targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < _angleOfView / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (Physics.Raycast(transform.position, directionToTarget, distanceToTarget, _obstructionMask) == false)
+                {
+                    _target = target;
+                }
+            }
+            else
+            {
+                _target = null;
+            }
+        }
+        else if (_target != null)
+        {
+            _target = null;
+        }
+
+        TargetHasBeenSighted?.Invoke(_target);
+    }
+}
